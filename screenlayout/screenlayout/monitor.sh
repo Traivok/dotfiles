9f7a69b3f8cb05d8349fc106edaf363d72ac497e
Traivok <jraf@cin.ufpe.br>
2018-02-23 17:37:20 -0300

    dev shortcuts

--- screenlayout/screenlayout/monitor.sh
+++ screenlayout/screenlayout/monitor.sh
@@ -2,10 +2,10 @@
 
 if xrandr | grep -q 'HDMI1 connected'; then
     echo "Dual monitor"
-    sh ~/.screenlayout/dual-monitor.sh
+    sh ~/.config/screenlayout/dual-monitor.sh
 else
     echo "Mono monitor"
-    sh ~/.screenlayout/mono-monitor.sh
+    sh ~/.config/screenlayout/mono-monitor.sh
 fi
 
 echo "Restoring wallpaper settings"
