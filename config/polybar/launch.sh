169f99ae1d27e8fc503eb809bb76dc74c64ae8e8
José Ricardo A. Figueirôa <jricardofigueiroa@gmail.com>
2021-01-07 11:06:24 -0300

    dots

---
+++ config/polybar/launch.sh
@@ -0,0 +1,12 @@
+# Basic script to kill all old bars and launch new.
+
+# Terminate already running bad instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while grep -x polybar >/dev/null; do sleep 1; done
+
+# Launch the main bar
+polybar main_bar
+
+if xrandr | grep -q 'HDMI-1 connected'; then polybar second_bar; fi
